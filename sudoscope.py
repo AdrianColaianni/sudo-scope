153032d45f7cf623625d539f56925769a90bd083
Adrian Colaianni <adrian@colaianni.us>
2025-10-29 15:56:48 -0400

    init

---
+++ sudoscope.py
@@ -0,0 +1,12 @@
+def read_input_files():
+    return
+
+def parse_file_content(input):
+    return
+
+def display_findings(input):
+    return
+
+input = read_input_files()
+output = parse_file_content(input)
+display_findings(output)
